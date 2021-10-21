2a93b51943e32e52032fc62db281e6521200a1b8
bardenha <bardenha@gmail.com>
2021-10-21 21:41:28 +0100

    config: deploy env checks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF" 
+echo "BRANCH_TO_BUILD: $BRANCH_TO_BUILD"
 
-if [[ "$VERCEL_ENV" == "production" ]] ; then
-  # Proceed with the build
+if [[ "$VERCEL_GIT_COMMIT_REF" == "$BRANCH_TO_BUILD" ]] ; then
   echo "✅ - Build can proceed"
   exit 1;
-
 else
-  # Don't build
-  echo "🛑 - Build cancelled"
+  echo "🛑 - Build canceled"
   exit 0;
 fi
