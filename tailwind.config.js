ddf9c8d20530fb7cbc14a40af6cd132776984787
bardenha <bardenha@gmail.com>
2021-10-22 21:32:13 +0100

    config: tailwind colors & fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        'Roboto',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+    },
+    extend: {
+      colors: {
+        primary: '#EF2A82',
+        secondary: '#0088F0',
+        base: '#172755',
+        chart: '#EEEEF6',
+        'primary-text': '#172755',
+        'secondary-text': '#8794BA',
+      },
+    },
   },
   variants: {
     extend: {},
