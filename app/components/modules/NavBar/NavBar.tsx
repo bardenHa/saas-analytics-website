bbf66acbffe55634577042ecf59b297d6093761e
bardenha <bardenha@gmail.com>
2021-10-23 14:35:21 +0100

    wip: navbar styling

---
+++ app/components/modules/NavBar/NavBar.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Logo from '/public/assets/logo/main.png';
+
+const NavBar = () => (
+  <header className="top-0 flex items-center w-full py-12 text-base text-chart">
+    <Image src={Logo} alt="analytics logo" width={184} height={40} />
+    <div className="flex-1 pl-20 space-x-10">
+      <button className="hover:underline">Products</button>
+      <button className="hover:underline">Pricing</button>
+      <button className="hover:underline">FAQ</button>
+      <button className="hover:underline">Blog</button>
+      <button className="hover:underline">About</button>
+    </div>
+    <div>
+      <button>Sign In</button>
+      <button>Sign Up</button>
+    </div>
+  </header>
+);
+
+export default NavBar;
