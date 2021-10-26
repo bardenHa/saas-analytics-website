9626d81389d079ed218a23cdc30d31950b0f08aa
bardenha <bardenha@gmail.com>
2021-10-26 18:30:13 +0100

    feat: alt footer logo

--- app/components/modules/Footer/Footer.tsx
+++ app/components/modules/Footer/Footer.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
+
 const Footer = () => {
   return (
     <footer className="bottom-0 px-4 text-base md:px-0 text-secondary-text">
       <div className="border-t border-divider-color"></div>
       <div className="flex items-center justify-between w-full mt-6 md:mt-12">
-        <div className="">footer</div>
+        <Image
+          src="/assets/logo/alt.png"
+          width={138}
+          height={30}
+          alt="analytics alternative logo"
+        />
         <div>links</div>
         <div>socials</div>
       </div>
